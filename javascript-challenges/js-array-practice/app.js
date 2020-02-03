60c74bb637130fcc7b0e3ec27362a113a57ba5a3
Sophie Szathmary-Dixon <Sophie.Szathmary-Dixon@Nology.local>
2020-02-03 12:45:09 +0000

    challenges and solutions

---
+++ javascript-challenges/js-array-practice/app.js
@@ -0,0 +1,24 @@
+const myArray = [12, 4, 26, 44, 1043, 2];
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
+
+//OR
+
+for (number of myArray) {
+    console.log(number);
+}
+
+//
+
+myArray.forEach((item) => {
+    console.log(item);
+});
+
+//OR
+
+// const print = (number) => {
+//     console.log(number);
+// }
+// myArray.forEach(print);
+
---
+++ javascript-challenges/js-array-practice/challenge.js
@@ -0,0 +1,32 @@
+const names = ["Benny", "Anna", "Bjorn", "Anna"];
+// 1. Loop through the names and print each one
+// for (number of names) {
+//     console.log(number);
+// }
+
+// 2. Print every letter from the second name in the list
+
+// [...names[1]].forEach((letter) => {
+//     console.log(letter);
+// });
+
+// 3. Loop through the array and print the length of each name
+
+names.forEach((names) => {
+        console.log(names.length);
+    });
+
+// 4. Use an array method to find the index of the name "Bjorn"
+
+// names.findIndex(("Bjorn") {
+//     console.log(index);
+
+let index = name.indexOf([2]);
+    console.log(index);
+
+// 5. Loop through the array and print each item in alphabetical order
+// EXTENSION
+    // 1. Sort the array by the length of the names, shortest to largest
+    // 2. Only return names that start with a "B"
+    // 3. Using the sort and reduce methods, return the word "ABBA" using the array
+    // 4. Using map, print each name adding "the legend" to the end of each.
